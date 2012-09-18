549f91a8061573cd8a7d99222b9b9c1f183c78d6
Adrian Felder <adrianfelder@gmx.ch>
2012-09-18 16:25:51 +0200

    Productlist Item rollover to show cart button

--- js/jquery.adon.js
+++ js/jquery.adon.js
@@ -4,39 +4,46 @@
  */
 
 (function(){
-  // if firefox 3.5+, hide content till load (or 3 seconds) to prevent FOUT
-  var d = document, e = d.documentElement, s = d.createElement('style');
-  if (e.style.MozTransform === ''){ // gecko 1.9.1 inference
-    s.textContent = '#slidesHome img{visibility:hidden}';
-    var r = document.getElementsByTagName('script')[0];
-    r.parentNode.insertBefore(s, r);
-    function f(){ s.parentNode && s.parentNode.removeChild(s); }
-    addEventListener('load',f,false);
-    setTimeout(f,2000); 
-  }
+    // if firefox 3.5+, hide content till load (or 3 seconds) to prevent FOUT
+    var d = document, e = d.documentElement, s = d.createElement('style');
+    if (e.style.MozTransform === ''){ // gecko 1.9.1 inference
+        s.textContent = '#slidesHome img{visibility:hidden}';
+        var r = document.getElementsByTagName('script')[0];
+        r.parentNode.insertBefore(s, r);
+        function f(){
+            s.parentNode && s.parentNode.removeChild(s);
+        }
+        addEventListener('load',f,false);
+        setTimeout(f,2000); 
+    }
 })();
 
-
 var $j = jQuery.noConflict();
-
 $j(document).ready(function(){
- $j('#slidesHome').cycle({
-fx: 'scrollHorz',
-timeout: 6000,
-delay: -2000 ,
-speed: 500,
-prev: '#prev',
-next: '#next',
-pager: '#navPager',
-pagerAnchorBuilder: pagerFactory
-});
-function pagerFactory(idx, slide) {
-var s = idx > 2 ? ' style="display:none"' : '';
-return '<li'+s+'><a href="#">'+(idx+1)+'</a></li>';
-}
+    $j('#slidesHome').cycle({
+        fx: 'scrollHorz',
+        timeout: 6000,
+        delay: -2000 ,
+        speed: 500,
+        prev: '#prev',
+        next: '#next',
+        pager: '#navPager',
+        pagerAnchorBuilder: pagerFactory
+    });
+    function pagerFactory(idx, slide) {
+        var s = idx > 2 ? ' style="display:none"' : '';
+        return '<li'+s+'><a href="#">'+(idx+1)+'</a></li>';
+    }
    
-    
-    
+    /* Animate Product Items in Listview (show cart button and compare link on hover */
+    $j(function() {
+        $j('.products-grid li.item').hover( function(){
+            $j(this).addClass('itemHover');
+        },
+        function(){
+            $j(this).removeClass('itemHover');
+        });
+    }); 
 });
 
 
